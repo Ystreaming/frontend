083ecef02cc244ad1e713230f6a97a8dfe08596d
Philippe VILARINHO <59869747+tifiloutube@users.noreply.github.com>
2023-10-19 16:29:01 +0200

    [FEAT] - Commencement de la home-page

--- src/app/home-videos/home-videos.component.ts
+++ src/app/home-videos/home-videos.component.ts
@@ -1,18 +1,32 @@
 import { Component } from '@angular/core';
 
+interface Categorie {
+  titre: string;
+  image: string;
+}
+
 @Component({
   selector: 'app-home-videos',
   templateUrl: './home-videos.component.html',
   styleUrls: ['./home-videos.component.scss']
 })
+
 export class HomeVideosComponent {
   // Image
   miniatureVideo = 'assets/miniatureVideo.jpeg';
   imageProfil = 'assets/moi.jpeg'
+
+  miniatureCategorie: Categorie[] = [
+    { titre: 'Gaming', image: 'assets/gaming.jpg' },
+    { titre: 'IRL', image: 'assets/irl.jpg' },
+    { titre: 'Podcast', image: 'assets/podcastv2.jpg' }
+  ];
 
   // Description
   miniatureDescription = 'miniature description';
   imageProfilDescription = 'image profil'
 
   videos = Array(3).fill(0);
+
+  hoveredIndex: number | null = null;  // Variable pour suivre l'index de l'élément survolé
 }
