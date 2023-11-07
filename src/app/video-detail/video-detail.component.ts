05073a49bef43e6d1739975560abfbef9968afd4
Philippe VILARINHO <59869747+tifiloutube@users.noreply.github.com>
2023-11-07 15:32:08 +0100

    [FEAT] - Ajout des commentaires

--- src/app/video-detail/video-detail.component.ts
+++ src/app/video-detail/video-detail.component.ts
@@ -49,4 +49,30 @@ export class VideoDetailComponent implements OnInit {
       console.error('Error loading video data', error);
     });
   }
+
+  getTimeSince(date: string) {
+    const now = new Date();
+    const past = new Date(date);
+    const msPerMinute = 60 * 1000;
+    const msPerHour = msPerMinute * 60;
+    const msPerDay = msPerHour * 24;
+    const msPerMonth = msPerDay * 30; // Approximation
+    const msPerYear = msPerDay * 365;
+
+    const elapsed = now.getTime() - past.getTime();
+
+    if (elapsed < msPerMinute) {
+      return 'il y a moins d\'une minute';
+    } else if (elapsed < msPerHour) {
+      return `il y a ${Math.round(elapsed/msPerMinute)} minute(s)`;
+    } else if (elapsed < msPerDay ) {
+      return `il y a ${Math.round(elapsed/msPerHour )} heure(s)`;
+    } else if (elapsed < msPerMonth) {
+      return `il y a ${Math.round(elapsed/msPerDay)} jour(s)`;
+    } else if (elapsed < msPerYear) {
+      return `il y a ${Math.round(elapsed/msPerMonth)} mois`;
+    } else {
+      return `il y a ${Math.round(elapsed/msPerYear )} an(s)`;
+    }
+  }
 }
