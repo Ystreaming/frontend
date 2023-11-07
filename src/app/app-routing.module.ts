64699da62aeddac86367428172256911505e0941
Philippe VILARINHO <59869747+tifiloutube@users.noreply.github.com>
2023-11-07 10:03:26 +0100

    [FEAT] - Mise en place du routage vers /video/:id

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
+import { VideoDetailComponent } from './video-detail/video-detail.component';
 
 const routes: Routes = [
-
+  { path: '', component: HomePageComponent }, // Route pour la page d'accueil
+  { path: 'video/:id', component: VideoDetailComponent }, // Route pour les détails de la vidéo
 ];
 
 @NgModule({
--- src/app/video-detail/video-detail.component.ts
+++ src/app/video-detail/video-detail.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-video-detail',
   templateUrl: './video-detail.component.html',
   styleUrls: ['./video-detail.component.scss']
 })
-export class VideoDetailComponent {
+export class VideoDetailComponent implements OnInit {
+  videoId: string | null = null;
 
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.videoId = this.route.snapshot.paramMap.get('id');
+    console.log('VideoDetailComponent est initialisé');
+  }
 }
