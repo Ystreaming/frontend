fbd48bec1422e5e1e9137c1e33fc647db5f578f3
Philippe VILARINHO <59869747+tifiloutube@users.noreply.github.com>
2023-11-09 14:43:20 +0100

    [FEAT] - Design register et login fini

--- src/app/pages/login-page/login-page.component.ts
+++ src/app/pages/login-page/login-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-login-page',
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./login-page.component.scss']
 })
 export class LoginPageComponent {
+  constructor(private el: ElementRef, private renderer: Renderer2) {}
 
+  onFocus(event: FocusEvent): void {
+    const target = event.target as HTMLElement;
+    this.renderer.setStyle(target, 'border-color', '#EC567C');
+    const label = target.previousElementSibling;
+    if (label) {
+      this.renderer.setStyle(label, 'color', '#EC567C');
+    }
+  }
+
+  onBlur(event: FocusEvent): void {
+    const target = event.target as HTMLElement;
+    this.renderer.removeStyle(target, 'border-color');
+    const label = target.previousElementSibling;
+    if (label) {
+      this.renderer.removeStyle(label, 'color');
+    }
+  }
 }
