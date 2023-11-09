188b1f62e8bc3098f5576621e822ab6e01ec6465
Philippe VILARINHO <59869747+tifiloutube@users.noreply.github.com>
2023-11-09 15:10:50 +0100

    [FIX] - rectification du style

--- src/app/pages/register-page/register-page.component.ts
+++ src/app/pages/register-page/register-page.component.ts
@@ -1,4 +1,4 @@
-import {Component, ElementRef, Renderer2} from '@angular/core';
+import { Component, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-register-page',
@@ -6,21 +6,21 @@ import {Component, ElementRef, Renderer2} from '@angular/core';
   styleUrls: ['./register-page.component.scss']
 })
 export class RegisterPageComponent {
-  constructor(private el: ElementRef, private renderer: Renderer2) {}
+  constructor(private renderer: Renderer2) {}
 
+  // Méthode pour gérer l'événement focus
   onFocus(event: FocusEvent): void {
     const target = event.target as HTMLElement;
-    this.renderer.setStyle(target, 'border-color', '#EC567C');
-    const label = target.previousElementSibling;
+    const label = target.previousElementSibling as HTMLElement;
     if (label) {
       this.renderer.setStyle(label, 'color', '#EC567C');
     }
   }
 
+  // Méthode pour gérer l'événement blur
   onBlur(event: FocusEvent): void {
     const target = event.target as HTMLElement;
-    this.renderer.removeStyle(target, 'border-color');
-    const label = target.previousElementSibling;
+    const label = target.previousElementSibling as HTMLElement;
     if (label) {
       this.renderer.removeStyle(label, 'color');
     }
