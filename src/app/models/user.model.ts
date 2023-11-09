144f1dff4dd1beb584357bd4b4a7b79e59c0bf60
DevLudo <60669196+DevLudo@users.noreply.github.com>
2023-11-09 09:19:00 +0100

    [FEAT] - Add user model

---
+++ src/app/models/user.model.ts
@@ -0,0 +1,13 @@
+export interface IUser {
+    id: number,
+    firstname: string,
+    lastname: string,
+    email: string,
+    dateOfBirth: string,
+    username: string,
+    password: string,
+    createdAt: string,
+    status: boolean,
+    language: string,
+    profileImage: string
+}
