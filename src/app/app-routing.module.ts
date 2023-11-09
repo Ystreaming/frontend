dda2d0c9690ce28d7679fc197d19f19c94d6f3d5
Philippe VILARINHO <59869747+tifiloutube@users.noreply.github.com>
2023-11-09 11:26:47 +0100

    [FEAT] - Création des formulaires de connexion et d'enregistrement

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { VideoPageComponent } from './pages/video-page/video-page.component';
+import { LoginPageComponent} from "./pages/login-page/login-page.component";
+import { RegisterPageComponent} from "./pages/register-page/register-page.component";
 
 const routes: Routes = [
   { path: '', component: HomePageComponent }, // Route pour la page d'accueil
   { path: 'video/:id', component: VideoPageComponent }, // Route pour les détails de la vidéo
+  { path: 'login', component: LoginPageComponent}, // Route pour la connexion
+  { path: 'register', component: RegisterPageComponent}, // Route pour l'inscription
 ];
 
 @NgModule({
