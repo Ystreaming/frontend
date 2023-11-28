5745d8ed30fd3983b1d88adc57c14aadd22fe564
DevLudo <60669196+DevLudo@users.noreply.github.com>
2023-11-28 16:15:43 +0100

    [FEAT] - Add guards to auth routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { IsSignedInGuard } from './guards/is-signed-in.guard';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomePageComponent } from './pages/home-page/home-page.component';
@@ -5,12 +6,13 @@ import { VideoPageComponent } from './pages/video-page/video-page.component';
 import { LoginPageComponent} from "./pages/login-page/login-page.component";
 import { RegisterPageComponent} from "./pages/register-page/register-page.component";
 import { ProfilPageComponent} from "./pages/profil-page/profil-page.component";
+import { IsNotSignedInGuard } from './guards/is-not-signed-in.guard';
 
 const routes: Routes = [
   { path: '', component: HomePageComponent }, // Route pour la page d'accueil
   { path: 'video/:id', component: VideoPageComponent }, // Route pour les détails de la vidéo
-  { path: 'login', component: LoginPageComponent}, // Route pour la connexion
-  { path: 'register', component: RegisterPageComponent}, // Route pour l'inscription
+  { path: 'login', canActivate: [IsNotSignedInGuard], component: LoginPageComponent}, // Route pour la connexion
+  { path: 'register', canActivate: [IsNotSignedInGuard], component: RegisterPageComponent}, // Route pour l'inscription
   { path: 'channel/:streamerName', component: ProfilPageComponent}, // Route pour la chaine
 ];
 
--- src/app/guards/is-signed-in.guard.ts
+++ src/app/guards/is-signed-in.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from "@angular/core";
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from "@angular/router";
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from "@angular/router";
 import { LocalStorageService } from "../services/local-storage.service";
 import { Observable } from "rxjs";
 
@@ -7,13 +7,13 @@ import { Observable } from "rxjs";
   providedIn: 'root'
 })
 export class IsSignedInGuard implements CanActivate {
-  constructor(private localStorageService: LocalStorageService) {}
+  constructor(private localStorageService: LocalStorageService, private router: Router) {}
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
     const token = this.localStorageService.get('token');
 
     if (token === null) {
-      // TODO: Redirect to login
+      this.router.navigate(['/login']);
     }
 
     return true;
