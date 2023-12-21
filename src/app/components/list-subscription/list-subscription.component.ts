24c2cd76222067d3e12a1ead75a77ff18289eb4f
DevLudo <60669196+DevLudo@users.noreply.github.com>
2023-12-21 09:10:40 +0100

    feat: add image on subscription list

--- src/app/components/list-subscription/list-subscription.component.ts
+++ src/app/components/list-subscription/list-subscription.component.ts
@@ -1,3 +1,4 @@
+import { environment } from 'src/environments/environment';
 import { Component, OnInit } from '@angular/core';
 import { LocalStorageService, MyJWTPayload } from 'src/app/services/local-storage.service';
 import { UserService } from 'src/app/services/user.service';
@@ -9,6 +10,7 @@ import { UserService } from 'src/app/services/user.service';
 })
 export class ListSubscriptionComponent implements OnInit {
   subscribers: any = [];
+  environment = environment;
 
   constructor(private localStorageService: LocalStorageService, private userService: UserService) {}
   
