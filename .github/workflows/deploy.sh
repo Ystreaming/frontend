1ed3385de0a75b3fbe7e504dd10196fe59903f77
MrBartou <53816747+MrBartou@users.noreply.github.com>
2024-01-23 16:10:46 +0100

    ci: add Github Action CI

---
+++ .github/workflows/deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Naviguer au répertoire de l'application
+cd frontend
+
+# Tirer les dernières modifications de la branche principale
+git pull origin main
+
+# Installez les dépendances (ajustez selon votre environnement de build)
+# Pour une application Node.js, par exemple :
+npm install
+
+# Construisez votre application (si nécessaire)
+# Par exemple, pour une application Node.js/React :
+npm run start
+
+echo "Déploiement terminé avec succès"
