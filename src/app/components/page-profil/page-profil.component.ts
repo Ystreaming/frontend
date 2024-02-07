d030394ee6d7e653915b9309daabb379442d902a
DevLudo <roux.ludovic.pro@gmail.com>
2024-02-07 17:14:03 +0100

    fix: subscribe button on profile page

--- src/app/components/page-profil/page-profil.component.ts
+++ src/app/components/page-profil/page-profil.component.ts
@@ -2,6 +2,9 @@ import { environment } from 'src/environments/environment';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ChannelService } from 'src/app/services/channel.service';
+import { LocalStorageService } from 'src/app/services/local-storage.service';
+import { UserService } from 'src/app/services/user.service';
+import { forkJoin, of } from 'rxjs';
 
 @Component({
   selector: 'app-page-profil',
@@ -13,18 +16,44 @@ export class PageProfilComponent implements OnInit {
   channelData: any;
   videos: any[] = [];
   environment = environment;
+  isSub: boolean = false;
 
-  constructor(private route: ActivatedRoute, private channelService: ChannelService) { }
+  constructor
+  (
+    private route: ActivatedRoute, private channelService: ChannelService,
+    private localStorageService: LocalStorageService, private userService: UserService
+  ) { }
 
   ngOnInit() {
     this.channelName = this.route.snapshot.paramMap.get('streamerName');
-    this.loadChannel();
+    this.loadData();
   }
 
-  loadChannel() {
-    this.channelService.searchChannelByUsername(this.channelName).subscribe(response => {
-      this.channelData = response.channel[0];
-      console.log(this.channelData);
+  loadData() {
+    let userId = this.localStorageService.getUserDetails();
+
+    forkJoin({
+      channelData: this.channelService.searchChannelByUsername(this.channelName),
+      subData: userId ? this.userService.getSubByUser(userId) : of(null),
+    }).subscribe(({ channelData, subData }) => {
+      this.channelData = channelData.channel[0];
+
+      if (subData) {
+        this.isSub = subData.subItems.some((sub: any) => sub._id === channelData.channel[0]._id);
+      }
+    });
+  }
+
+  subscribe() {
+    let userId = this.localStorageService.getUserDetails();
+
+    if (userId === null) {
+      return;
+    }
+    
+    let idChannel = this.channelData._id;
+    this.userService.createSub(idChannel).subscribe(() => {
+      this.ngOnInit();
     })
   }
 }
